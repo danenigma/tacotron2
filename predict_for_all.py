37154eb05d4d14952f5827d03f0a7e0c660fdc42
danenigma <danielfekaduenigma@yahoo.com>
2018-12-02 12:50:48 -0500

    for all files

--- predict_for_all.py
+++ predict_for_all.py
@@ -1,11 +1,19 @@
 from client import *
+import os 
+import csv
 
-files = ['arctic_a0001.wav']
+audio_dir = 'cmu_us_bdl_arctic/wav/'
+audio_files  = os.listdir(audio_dir)
+
 model = 'models/output_graph.pbmm'
 alphabet = 'models/alphabet.txt'
 lm = 'models/lm.binary'
 trie =  'models/trie'
-
-for audio in files:
-	predict(model, alphabet, lm, trie, audio)
-		
+f = open('result.csv', 'w')
+ 
+for idx, audio in enumerate(audio_files):
+	text   = predict(model, alphabet, lm, trie, os.path.join(audio_dir, audio))
+	writer = csv.writer(f)
+	writer.writerow([str(idx), text])
+	print('Done convering audio :', idx)
+	
